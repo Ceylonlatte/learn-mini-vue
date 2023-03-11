d3c2a1c9632a0fea7f670ad4cee1c7db53255a43
liuxinpei <123546908@qq.com>
2023-03-11 21:09:30 +0800

    init project

---
+++ src/effect.ts
@@ -0,0 +1,106 @@
+import { extend } from "./shared";
+
+const targetMap = new WeakMap();
+let activeEffect;
+let shouldTrack;
+
+class ReactiveEffect {
+  private _fn: any;
+  deps = [];
+  active = true;
+  onStop?:()=> void;
+  constructor(fn, private scheduler) {
+    this._fn = fn;
+  }
+  run() {
+   
+    if(!this.active) {
+     return this._fn()
+    }
+    shouldTrack = true
+    activeEffect = this;
+    const result = this._fn()
+
+    // reset 
+    shouldTrack = false
+
+    return result;
+  }
+  stop() {
+    if(this.active) {
+      cleanupEffect(this);
+      if(this.onStop) {
+        this.onStop()
+      }
+      this.active = false;
+    }
+   
+  }
+}
+
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect);
+  });
+}
+
+export function track(target, key) {
+  if(!isTracking()) return; 
+
+
+  let depsMap = targetMap.get(target);
+
+  if (!depsMap) {
+    depsMap = new Map();
+
+    targetMap.set(target, depsMap);
+  }
+
+  let dep = depsMap.get(key);
+
+  if (!dep) {
+    dep = new Set();
+    depsMap.set(key, dep);
+  }
+
+  // 如果activeEffect已经在dep中不需要重复收集
+  if(dep.has(activeEffect)) return;
+  
+  dep.add(activeEffect);
+  activeEffect.deps.push(dep);
+}
+
+function isTracking() {
+  return shouldTrack && activeEffect !== undefined
+}
+
+export function trigger(target, key) {
+  const depsMap = targetMap.get(target);
+
+  const deps = depsMap.get(key);
+
+  for (const effect of deps) {
+    if (effect.scheduler) {
+      effect.scheduler();
+    } else {
+      effect.run();
+    }
+  }
+}
+
+export function effect(fn, options: any = {}) {
+  const _effect = new ReactiveEffect(fn, options.scheduler);
+  // extend
+  extend(_effect, options)
+  
+  _effect.run();
+
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
+
+  return runner; // 改变this指向当前的_effect实例
+}
+
+export function stop(runner) {
+  runner.effect.stop();
+}
---
+++ src/reactivity/tests/effect.spec.ts
@@ -0,0 +1,110 @@
+import { reactive, effect } from "../../index";
+import { stop } from "../../effect";
+
+describe("effect", () => {
+  it("happy path", () => {
+    const user = reactive({
+      age: 10,
+    });
+
+    let nextAge;
+
+    effect(() => {
+      nextAge = user.age + 1;
+    });
+    expect(nextAge).toBe(11);
+    // // update
+    user.age++;
+    expect(nextAge).toBe(12);
+  });
+
+  it("should return runner", () => {
+    // 1. effect => return function(runner) => fn => reutrn 调用effect会返回内部_effect.run方法，_effect.run又会返回effect的返回值
+
+    let foo = 10;
+    const runner = effect(() => {
+      foo++;
+      return "foo";
+    });
+
+    expect(foo).toBe(11);
+    const r = runner();
+    expect(foo).toBe(12);
+    expect(r).toBe("foo");
+  });
+
+  /**
+   * 当响应式值发生改变，不是执行effect方法而是执行传入的scheduler
+   */
+  it("scheduler", () => {
+    // 1. 通过effect的第二个参数给定一个scheduler的fn
+    // 2. effect 第一次初始化的时候，还会执行fn
+    // 3. 当响应式对象set 发生改变不会执行fn 而是执行scheduler
+    // 4. 当执行runner的时候， 会再次执行fn
+
+    let dummy;
+    let run: any;
+    const scheduler = jest.fn(() => {
+      run = runner;
+    });
+    const obj = reactive({ foo: 1 });
+
+    const runner = effect(
+      () => {
+        dummy = obj.foo;
+      },
+      { scheduler }
+    );
+
+    expect(scheduler).not.toHaveBeenCalled(); // 不会被调用
+    expect(dummy).toBe(1);
+    // should be called on first trigger
+    obj.foo++;
+    expect(scheduler).toHaveBeenCalledTimes(1); // 这里验证当响应式值发生改变，不是执行effect方法而是执行传入的scheduler
+    // // should not run yey
+    expect(dummy).toBe(1);
+    // // manually run
+    run();
+    // // should have run
+    expect(dummy).toBe(2);
+  });
+
+  /**
+   * 调用stop时，当响应式数据改变时不会执行effect
+   */
+  it("stop", () => {
+    let dummy;
+    const obj = reactive({ prop: 1 });
+    const runner = effect(() => {
+      dummy = obj.prop;
+    });
+    obj.prop = 2;
+    expect(dummy).toBe(2);
+    stop(runner);
+    obj.prop++;
+    expect(dummy).toBe(2);
+
+    // stopped effect should still be manually callable
+    runner();
+    expect(dummy).toBe(3);
+  });
+
+  /**
+   * 调用stop时，会执行onStop回调
+   */
+  it("onStop", () => {
+    const obj = reactive({ foo: 1 });
+    const onStop = jest.fn();
+    let dummy;
+
+    const runner = effect(
+      () => {
+        dummy = obj.foo;
+      },
+      { onStop }
+    );
+    stop(runner);
+
+    expect(onStop).toBeCalledTimes(1);
+  });
+});
