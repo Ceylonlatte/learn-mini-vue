03995332738dbff31e0b5ea6c46a881568bbb567
liuxinpei <123546908@qq.com>
2023-05-03 17:49:11 +0800

    fix: renderer bug

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -53,8 +53,23 @@ function setupStatefulComponent(instance: any) {
   }
 }
 
-function handleSetupResult(instance, setupResult: any) {
-  if (typeof setupResult === "object") {
+function handleSetupResult(instance, setupResult) {
+  // setup 返回值不一样的话，会有不同的处理
+  // 1. 看看 setupResult 是个什么
+  if (typeof setupResult === "function") {
+    // 如果返回的是 function 的话，那么绑定到 render 上
+    // 认为是 render 逻辑
+    // setup(){ return ()=>(h("div")) }
+    instance.render = setupResult;
+  } else if (typeof setupResult === "object") {
+    // 返回的是一个对象的话
+    // 先存到 setupState 上
+    // 先使用 @vue/reactivity 里面的 proxyRefs
+    // 后面我们自己构建
+    // proxyRefs 的作用就是把 setupResult 对象做一层代理
+    // 方便用户直接访问 ref 类型的值
+    // 比如 setupResult 里面有个 count 是个 ref 类型的对象，用户使用的时候就可以直接使用 count 了，而不需要在 count.value
+    // 这里也就是官网里面说到的自动结构 Ref 类型
     instance.setupState = proxyRefs(setupResult);
   }
 
@@ -62,11 +77,25 @@ function handleSetupResult(instance, setupResult: any) {
 }
 
 function finishComponentSetup(instance) {
+  // 给 instance 设置 render
+
+  // 先取到用户设置的 component options
   const Component = instance.type;
-  if(compile && !Component.render) {
-    Component.render = compile(Component.template)
+
+  if (!instance.render) {
+    // 如果 compile 有值 并且当组件没有 render 函数，那么就需要把 template 编译成 render 函数
+    if (compile && !Component.render) {
+      if (Component.template) {
+        // 这里就是 runtime 模块和 compile 模块结合点
+        const template = Component.template;
+        Component.render = compile(template);
+      }
+    }
+
+    instance.render = Component.render;
   }
-  instance.render = Component.render;
+
+  // applyOptions()
 }
 
 let currentInstance: any = null;
